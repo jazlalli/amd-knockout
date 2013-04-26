42b3f853202b4f05692b5ccc15529159d0e2f73d
Jaz <jazlalli@hotmail.com>
2013-04-26 16:16:40 +0100

    tidying up

--- AMDKnockout/Public/script/app.js
+++ AMDKnockout/Public/script/app.js
@@ -15,8 +15,9 @@
             initialize: function () {
                 var self = this;
 
+                // TODO Wrap up controllers in page level controller
+                //define the controllers global namespace and attach appropriate controllers
                 self.Controller = self.Controller || {};
-                
                 self.Controller.CardsTable = new CardsTableController();
                 self.Controller.Categories = new CategoriesController();
                 self.Controller.Calculator = new CalculatorController();
@@ -25,6 +26,8 @@
             bootstrap: function () {
                 var self = this;
 
+                // TODO Bind viewmodel using data attribute instead of hard-coded DOM selector
+                // binds viewmodel to DOM element
                 ko.applyBindings(self.Controller.Categories.viewModel, $('#categories')[0]);
                 ko.applyBindings(self.Controller.Calculator.viewModel, $('#calculators')[0]);
                 ko.applyBindings(self.Controller.CardsTable.viewModel, $('#cardstable')[0]);
